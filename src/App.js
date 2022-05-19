f882f6a26cfd249e6247ab33197c4d7c9bb0221e
rezenderegis <rezenderegis@gmail.com>
2022-05-19 07:51:52 -0300

    Connect Wallet Symple

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ function App() {
 
   const connectWallet = async () => {
 
+    //2. UseState - just start and will complete
+    const [getWalletAddress, setWalletAddress] = useState(null);
 
 
 
-    //Exactly the cole of Metamask Documentation 
+    //Exactly the code of Metamask Documentation 
     //https://docs.metamask.io/guide/getting-started.html#basic-considerations
 
     if (typeof window.ethereum !== 'undefined') {
 
+      /*1 - We have the accounts. Now we need to save.
+      Using use State
+      */
       const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
 
-      debugger
+      
     }
 
   }
